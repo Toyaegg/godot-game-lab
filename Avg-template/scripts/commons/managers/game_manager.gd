1a786a438bb5fc84b47f935b5b06ca02c488e7a9
HuangKai <1750645605@qq.com>
2025-08-15 17:47:05 +0800

    .

---
+++ Avg-template/scripts/commons/managers/game_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+class_name GameManager
+
+func _ready() -> void:
+	var inital:PackedScene = load("res://scenes/gui/initalization.tscn")
+	# 初始化UIManager
+	# TODO 添加UIManager
+	# 打开初始化界面，播放开发者logo
+	add_child(inital.instantiate())
+	# 初始化GlobalDataManager
+	# TODO 添加GlobalDataManager
+	
--- Avg-template/test/node_2d.gd
+++ Avg-template/test/node_2d.gd
@@ -3,4 +3,8 @@ extends Node2D
 func _ready() -> void:
 	var resource = load("res://test/test.dialogue")
 	
-	DialogueManager.show_dialogue_balloon(resource, "start")
+	var balloon = DialogueManager.show_dialogue_balloon(resource, "测试") as CanvasLayer
+	DialogueManager.dialogue_ended.connect(func(arg):
+		print("结束了", arg, balloon)
+		#balloon.hide()
+	)
